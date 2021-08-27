8bb451a78956911d36143abb2e3adf3e116d14a3
Ashish Sharda <ashishjsharda@gmail.com>
2021-08-27 08:27:06 -0700

    Add all my files

---
+++ src/main/java/com/ashish/passwordencoder/demo/RestApiDemoController.java
@@ -0,0 +1,12 @@
+package com.ashish.passwordencoder.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestApiDemoController {
+    private List<Coffee> coffeeList=new ArrayList<>();
+
+}
