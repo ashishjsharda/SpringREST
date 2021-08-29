a8583a08d921653611335b0f2513b4cd3e32f1c2
Ashish Sharda <ashishjsharda@gmail.com>
2021-08-29 15:13:31 -0700

    Add all my files

--- src/main/java/com/ashish/passwordencoder/demo/RestApiDemoController.java
+++ src/main/java/com/ashish/passwordencoder/demo/RestApiDemoController.java
@@ -1,12 +1,28 @@
 package com.ashish.passwordencoder.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/")
 public class RestApiDemoController {
     private List<Coffee> coffeeList=new ArrayList<>();
+    public RestApiDemoController(){
+        coffeeList.addAll(List.of(
+                new Coffee("Nescafe"),
+                new Coffee("Bru Cafe"),
+                new Coffee("Suzu's Cafe")
+        ));
+    }
+
+    @GetMapping("/coffees")
+    Iterable<Coffee>getCoffes(){
+        return coffeeList;
+    }
 
 }
