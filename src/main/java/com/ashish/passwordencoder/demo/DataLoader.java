d7fca61d2c82f6b29bc068b206976e6de1610dfc
Ashish Sharda <ashishjsharda@gmail.com>
2021-09-25 21:38:53 -0400

    Add all my files

---
+++ src/main/java/com/ashish/passwordencoder/demo/DataLoader.java
@@ -0,0 +1,26 @@
+package com.ashish.passwordencoder.demo;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+public class DataLoader {
+    private final CoffeeRepository coffeeRepository;
+
+    public DataLoader(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+    }
+
+    @PostConstruct
+    private void loadData()
+    {
+        coffeeRepository.saveAll(List.of(
+                new Coffee("Café Cereza"),
+                new Coffee("Café Ganador"),
+                new Coffee("Café Lareño"),
+                new Coffee("Café Três Pontas")
+        ));
+    }
+}
