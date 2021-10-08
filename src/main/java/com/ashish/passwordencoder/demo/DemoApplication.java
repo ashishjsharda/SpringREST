c910846a58c349cc3cc92750549467d9aa6a5940
Ashish Sharda <ashishjsharda@gmail.com>
2021-10-08 06:09:55 -0400

    Add all my files

--- src/main/java/com/ashish/passwordencoder/demo/DemoApplication.java
+++ src/main/java/com/ashish/passwordencoder/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.ashish.passwordencoder.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.UUID;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DemoApplication {
 
     public static void main(String[] args) {
