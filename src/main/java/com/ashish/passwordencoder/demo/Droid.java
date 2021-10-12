0afa0d4e381d0390d382fe184a581523d21733e3
Ashish Sharda <ashishjsharda@gmail.com>
2021-10-12 05:43:22 -0400

    Add all my files

---
+++ src/main/java/com/ashish/passwordencoder/demo/Droid.java
@@ -0,0 +1,23 @@
+package com.ashish.passwordencoder.demo;
+
+public class Droid {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
